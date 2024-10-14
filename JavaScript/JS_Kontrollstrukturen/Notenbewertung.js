ddb84655ed93dce2b3bfe185d9ed9f4c3a65d3f6
Marcel Ebel <ebelmarcel@gmail.com>
2024-10-14 13:39:11 +0200

    14.10.2024

---
+++ JavaScript/JS_Kontrollstrukturen/Notenbewertung.js
@@ -0,0 +1,56 @@
+// Funktion, die eine zufällige Note zwischen 1 und 6 zurückgibt
+function getGrade() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+// Funktion, die die Note bewertet
+function checkGrade() {
+    const grade = getGrade();
+    console.log("Deine Note ist: " + grade);
+
+    // Bewertung der Note mit switch-case
+    /*
+    switch (grade) {
+        case 1:
+            console.log("Sehr gut");
+            break;
+        case 2:
+            console.log("Gut");
+            break;
+        case 3:
+            console.log("Befriedigend");
+            break;
+        case 4:
+            console.log("Ausreichend");
+            break;
+        case 5:
+            console.log("Mangelhaft");
+            break;
+        case 6:
+            console.log("Nicht bestanden");
+            break;
+        default:
+            console.log("Ungültige Note");
+    }
+    */
+
+    // Bewertung der Note mit if-else
+    if (grade == 1) {
+        console.log("Sehr gut");
+    } else if (grade == 2) {
+        console.log("Gut");
+    } else if (grade == 3) {
+        console.log("Befriedigend");
+    } else if (grade == 4) {
+        console.log("Ausreichend");
+    } else if (grade == 5) {
+        console.log("Mangelhaft");
+    } else if (grade == 6) {
+        console.log("Nicht bestanden");
+    } else {
+        console.log("Ungültige Note");
+    }
+}
+
+// Funktionsaufruf
+checkGrade();
